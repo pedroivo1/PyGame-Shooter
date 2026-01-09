848c966caff5b00044c04627890231219a8eb852
pedroivo1 <pedroivoal1@gmail.com>
2026-01-08 22:55:09 -0300

    v2

--- main.py
+++ main.py
@@ -1,15 +1,41 @@
 import pygame
 
+BG = (144, 201, 120)
+def draw_bg():
+    screen.fill(BG)
+
 class Soldier(pygame.sprite.Sprite):
-    def __init__(self, x, y, scale):
+    def __init__(self, char_type, x, y, scale, speed):
         super().__init__()
-        img = pygame.image.load('img/player/Idle/0.png')
+        self.char_type = char_type
+        self.speed = speed
+        self.direction = 1
+        self.flip = False
+
+        img = pygame.image.load(f'img/{self.char_type}/Idle/0.png')
         self.image = pygame.transform.scale(img, (int(img.get_width()*scale), int(img.get_height()*scale)))
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
 
+
     def draw(self):
-        screen.blit(self.image, self.rect)
+        screen.blit(pygame.transform.flip(self.image, self.flip, False), self.rect)
+
+    def move(self, moving_left, moving_right):
+        dx = 0
+        dy = 0
+
+        if moving_left:
+            dx -= self.speed
+            self.flip = True
+            self.direction = -1
+        if moving_right:
+            dx += self.speed
+            self.flip = False
+            self.direction = 1
+
+        self.rect.x += dx
+        self.rect.y += dy
 
 if __name__ == '__main__':
     pygame.init()
@@ -18,19 +44,43 @@ if __name__ == '__main__':
     SCREEN_WIDTH = 800
     SCREEN_HEIGTH = int(SCREEN_WIDTH *0.8)
 
+    clock = pygame.time.Clock()
+    FPS = 60
+
+    moving_left = False
+    moving_right = False
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGTH))
     pygame.display.set_caption('Shooter')
 
-    player = Soldier(200, 200, 3)
+    player = Soldier('player', 200, 200, 3, 5)
+    enemy = Soldier('enemy', 200, 200, 3, 5)
 
     run = True
     while run:
+        clock.tick(FPS)
 
+        draw_bg()
         player.draw()
+        player.move(moving_left, moving_right)
+
+        enemy.draw()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_a:
+                    moving_left = True
+                if event.key == pygame.K_d:
+                    moving_right = True
+                if event.key == pygame.K_ESCAPE:
+                    run = False
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_a:
+                    moving_left = False
+                if event.key == pygame.K_d:
+                    moving_right = False
 
         pygame.display.update()
 
